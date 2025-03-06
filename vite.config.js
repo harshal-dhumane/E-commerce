54bf1e3ad14e154283d5c8746e2026afb6488b72
Harshal Dhumane <harshaldhumane53@gmail.com>
2025-03-06 23:03:07 +0530

    Updated vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/E-commerce/", // 👈 Add this lin
-
-})
+  base: "/E-commerce/", // 👈 Add this line
+});
